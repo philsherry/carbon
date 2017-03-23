8d6cb1695c7c201ba55bcae32bfd570b5ba645d6
Toni-Leigh Sharpe <toni.leigh@sage.com>
2017-03-23 09:43:32 +0000

    QA commit DO NOT MERGE

--- src/components/show-edit-pod/show-edit-pod.js
+++ src/components/show-edit-pod/show-edit-pod.js
@@ -240,7 +240,7 @@ class ShowEditPod extends React.Component {
     return (
       <Form
         afterFormValidation={ this.onSaveEditForm }
-        beforeFormValidation={ this.beforeFormValidation }
+        beforeFormValidation={ this.props.beforeFormValidation }
         buttonAlign={ this.props.buttonAlign }
         cancel={ this.props.cancel }
         cancelText={ this.props.cancelText }
